0275670d70d0986a699c0e303090657e9f9bc493
DUO13EH\Eric <wkhoeric@netvigator.com>
2017-05-31 23:40:09 +0800

    Commit 2

--- WFApp1/Form1.Designer.cs
+++ WFApp1/Form1.Designer.cs
@@ -37,6 +37,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "ABC123";
             // 
             // Form1
             // 
